bffecf52a8be3711effb68ea97359754c2323d65
sgbliznyuk <sgbliznyuk@gmail.com>
2020-01-27 14:12:37 +0300

    added links styling in navi pages

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,47 +4,47 @@ import config from '../../data/SiteConfig'
 
 export default (props) => (
     <div className="navigation-wrapper"> 
-    <div className="hiwords">
-        <div className="text-wrapper">
-            Hello, I`m Simon, full-stack software engineer <span role="img" aria-label="wip">⛏️</span>, working on <a href={config.github} target="_blank" rel="noopener noreferrer">open-source</a> and writing about my vision to web-development.<span role="img" aria-label="paper">🧾</span>
+        <div className="hiwords">
+            <div className="text-wrapper">
+                Hello, I`m Simon, full-stack software engineer <span role="img" aria-label="wip">⛏️</span>, working on <a href={config.github} target="_blank" rel="noopener noreferrer">open-source</a> and writing about my vision to web-development.<span role="img" aria-label="paper">🧾</span>
+            </div>
         </div>
-    </div>
-    <div className="navigation">
-        <StaticQuery
-            query={graphql`
-            query TagsList {
-                allMarkdownRemark {
-                distinct(field: frontmatter___tags)
+        <div className="navigation">
+            <StaticQuery
+                query={graphql`
+                query TagsList {
+                    allMarkdownRemark {
+                    distinct(field: frontmatter___tags)
+                    }
                 }
-            }
-            `} 
-            render={data => (
-                <div className="navigation-menu-tags">
-                    <pre>[</pre>
-                    <pre> {'{'}</pre>
-                    <pre>   <span className="tags-list-header">tags</span>: [</pre>
-                    {data.allMarkdownRemark.distinct
-                    .map(tag => (
-                    <pre>      <Link to={tag} activeClassName="link-active" key={tag}>"{tag}"</Link>,</pre>
-                    ))}
-                    <pre>      ],</pre>
-                    <pre> {'}'},</pre>
-                </div>
-            )}
-        />
+                `} 
+                render={data => (
+                    <div className="navigation-menu-tags">
+                        <pre>[</pre>
+                        <pre> {'{'}</pre>
+                        <pre>   <span className="tags-list-header">tags</span>: [</pre>
+                        {data.allMarkdownRemark.distinct
+                        .map(tag => (
+                        <pre>      <Link to={tag} activeClassName="link-active" key={tag}>"{tag}"</Link>,</pre>
+                        ))}
+                        <pre>      ],</pre>
+                        <pre> {'}'},</pre>
+                    </div>
+                )}
+            />
         <div className="navigation-menu-links">
-            {props.pages.map(page => (
-                <div className="page-link" key={page.name}>
-                    <pre> <Link activeClassName="link-active" to={page.link} key={page.name}>{page.name}</Link>,</pre>
-                </div>
-            ))}
-            <pre>]</pre>
+                {props.pages.map(page => (
+                    <div className="page-link" key={page.name}>
+                        <pre> <Link activeClassName="link-active" to={page.link} key={page.name}>{page.name}</Link>,</pre>
+                    </div>
+                ))}
+                <pre>]</pre>
+            </div>
         </div>
-    </div>
-    <div className="newsletter">
+        <div className="newsletter">
         <div className="text-wrapper">
             You can get materials directly from me by email. <span role="img" aria-label="mailbox">📬</span> 
-            I`ll never send you a spam. <span role="img" aria-label="letter">📨</span> Subscribe to newsletter.
+            I`ll never send you a spam. <span role="img" aria-label="letter">📨</span> <a href="">Subscribe to newsletter.</a> 
         </div>
     </div>
     </div>
