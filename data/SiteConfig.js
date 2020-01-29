291887edffa82dc0f03f9909e37b480fb19dcc1f
sgbliznyuk <sgbliznyuk@gmail.com>
2020-01-29 09:59:00 +0300

    added typography to main page

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,21 +1,15 @@
 const config = {
-    siteTitle: 'Simon Bliznyuk',
+    siteTitle: 'Simon Bliznyuk`s dev blog',
     siteTitleShort: 'Simon Bliznyuk',
     siteTitleAlt: 'Simon Bliznyuk',
-    // siteLogo: '/logos/logo-1024.png',
     siteUrl: 'https://www.simonbliznyuk.com/',
-    repo: 'https://github.com/simonbliznyuk/devblog',
-    github: 'https://github.com/simonbliznyuk',
+    repo: 'https://github.com/sbliznyuk/devblog',
+    github: 'https://github.com/sbliznyuk',
     pathPrefix: '',
     dateFromFormat: 'YYYY-MM-DD',
     dateFormat: 'MMMM Do, YYYY',
     siteDescription:
       'I`m Simon Bliznyuk, full-stack software engineer, working on open-source and writing about web-development.',
-    // siteRss: '/rss.xml',
-    // googleAnalyticsID: 'UA-42068444-1',
-    // postDefaultCategoryID: 'Tech',
-    // newsletter: 'https://taniarascia.substack.com',
-    // newsletterEmbed: 'https://taniarascia.substack.com/embed',
     userName: 'Simon',
     userEmail: 'hello@simonbliznyuk.com',
     userTwitter: 'simonbliznyuk',
@@ -33,8 +27,6 @@ const config = {
         link: '/contacts',
       },
     ],
-    // themeColor: '#3F80FF', // Used for setting manifest and progress theme colors.
-    // backgroundColor: '#ffffff',
   }
   
 module.exports = config
