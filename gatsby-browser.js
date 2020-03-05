dc0e95631839cba1a9c26ce5a4402b5941c8b9b1
sgbliznyuk <sgbliznyuk@gmail.com>
2020-03-05 11:36:05 +0300

    took out code theme to dedicated file

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,5 @@
 require("prismjs/plugins/command-line/prism-command-line.css")
 require("prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.js")
 require("prismjs/plugins/show-language/prism-show-language.js")
-require("prismjs/themes/prism-okaidia.css")
-// require("prismjs/themes/prism-coy.css")
-// require("./src/styles/prism-atom-dark.css")
+// require("prismjs/themes/prism-okaidia.css")
+require("./src/styles/base/prism-mokaidia.css")
